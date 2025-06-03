152997d1bf069230441db3a79d1969855572d458
Nick Porcino <meshula@hotmail.com>
2025-06-02 18:05:01 -0700

    Remove unneeded includes

--- src/Gui.h
+++ src/Gui.h
@@ -4,13 +4,13 @@
 
 /// ImGui includes
 #include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+//#include "imgui_impl_glfw.h"
+//#include "imgui_impl_opengl3.h"
 #include "imgui_internal.h"
 #include "imgui_stdlib.h"
 
 /// glfw
-#include <GLFW/glfw3.h>
+//#include <GLFW/glfw3.h>
 
 /// Icons font
 #include "IconsFontAwesome5.h"
